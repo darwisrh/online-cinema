0dd44a5086f700ca23a65f23a0392aff6042cb7b
drslevi2908@gmail.com <drslevi2908@gmail.com>
2023-02-20 11:01:18 +0700

    online cinema

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl underline text-red-600 hover:text-blue-700 cursor-pointer">Hello World</h1>
+    </>
+  )
+}
+
+export default App;
